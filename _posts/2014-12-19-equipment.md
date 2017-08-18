6af1b508a311241b384a5a435ea57e762977f1d5
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2017-08-18 17:45:41 -0400

    big map update!

--- _posts/2014-12-19-equipment.md
+++ _posts/2014-12-19-equipment.md
@@ -3,44 +3,28 @@ layout: default
 title: The Kitchen
 permalink: kitchen.html
 ---
+<link rel="stylesheet" href="{{ site.baseurl }}/css/mapplic/mapplic.css" >
+<link rel="stylesheet" href="{{ site.baseurl }}/css/mapplic/magnific-popup.css" >
+<link rel="stylesheet" href="{{ site.baseurl }}/css/mapplic/map.css" >
+<style>
+.mapplic-sidebar {
+	width: 25%;
+}
 
-<div class="row"><div class="col-md-offset-2 col-md-8"><img src="images/map.png" alt="Kitchen Layout" class="img-responsive" /></div></div>
+.mapplic-container {
+	width: 75%;	
+}
+.mapplic-clickable:not(g), g.mapplic-clickable > *{
+	opacity: .75;
+}
+.mapplic-clickable:not(g):hover, g.mapplic-clickable:hover > *{
+	opacity: 1;
+}
+</style>
 
-### [Green Kitchen](/tours/green) ###
-{: .col-md-offset-1}
-
-- Gas range: 6-burner with 24" griddle
-- Convection oven
-- Conventional oven
-{: .col-md-offset-1 .columns}
-
-### [Red Kitchen](/tours/red) ###
-{: .col-md-offset-1}
-
-- Gas range: 6-burner
-- Double-stacked convection oven
-- Conventional oven
-- Steamer
-{: .col-md-offset-1 .columns}
-
-### [Yellow Kitchen](/tours/yellow) ###
-{: .col-md-offset-1}
-
-- Gas range: 6-burner with 24" griddle
-- Convection oven
-- Conventional oven
-- Vegetable chopper
-{: .col-md-offset-1 .columns}
-
-### Misc. ###
-{: .col-md-offset-1}
-
-- Commercial dishwashing machine
-- 3 basin sink
-- Chilled, frozen, and [dry](/tours/dry) storage
-- Ice machine
-- Stainless steel work stations (stationary & wheeled)
-- Vacuum sealer
-- Mobile warming unit
-- Hand washing sinks
-{: .col-md-offset-1 .columns}
+<div class="row">
+	<div class="col-md-12" >
+		<div id="Map">
+		</div>
+	</div>
+</div>
--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
-var $j = jQuery.noConflict()
-$j(function() {
+(function() {
 	if(window.location.href.indexOf("nonav")>0){
-		$j(".navbar,.purechat-widget,.chat").remove();
+		$(".navbar,.purechat-widget,.chat").remove();
 	}
 	
-	if($j("#member-data").length){
-		$j.getJSON("https://api.airtable.com/v0/appf7AikwGn1IfZMA/CookSpring%20Members?api_key=keycxV47OKNroS5ID&maxRecords=9999&view=Current%20Members%20(Public)",function(v){
+	if($("#member-data").length){
+		$.getJSON("https://api.airtable.com/v0/appf7AikwGn1IfZMA/CookSpring%20Members?api_key=keycxV47OKNroS5ID&maxRecords=9999&view=Current%20Members%20(Public)",function(v){
 			var r = v.records.sort(function(){return 0.5 - Math.random() });
-			$j.each(r,function(i,el){
+			$.each(r,function(i,el){
 				if(el.fields.Logo){
-					$j("#member-data").append("<div class='member col-xs-6 col-md-3'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
+					$("#member-data").append("<div class='member col-xs-6 col-md-3'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
 				}
 			});			
 		});
 	}
-	if($j("#temp-data").length){
-		$j.getJSON("https://www.imonnit.com/json/SensorList/dGhlc3VtbWl0OkNoYXNwMTAxMA==",function(v){
-			$j.each(v.Result,function(i,el){
-				$j("#temp-data").append("<h1>" + el.SensorName + "</h1>");
-				$j("#temp-data").append("<h2>" + el.CurrentReading + "</h2>");
+	if($("#temp-data").length){
+		$.getJSON("https://www.imonnit.com/json/SensorList/dGhlc3VtbWl0OkNoYXNwMTAxMA==",function(v){
+			$.each(v.Result,function(i,el){
+				$("#temp-data").append("<h1>" + el.SensorName + "</h1>");
+				$("#temp-data").append("<h2>" + el.CurrentReading + "</h2>");
 			})
 		});
 	};
+});
+var map = "";
+$(document).ready(function(){
+	var h = $(window).height() - $(".navbar-brand img").height() - 75;
+	map = $("#Map").mapplic({
+		sidebar: true,
+		height: h,
+		markers: false,
+		fullscreen: false,
+		fillcolor: false,
+		hovertip: false
+	});
 });
