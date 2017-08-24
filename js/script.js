7eb2e8c67b5dec72404ae3c5d78ed2a4999a2a07
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2017-08-24 14:22:49 -0400

    members page

--- js/script.js
+++ js/script.js
@@ -3,16 +3,7 @@
 		$(".navbar,.purechat-widget,.chat").remove();
 	}
 	
-	if($("#member-data").length){
-		$.getJSON("https://api.airtable.com/v0/appf7AikwGn1IfZMA/CookSpring%20Members?api_key=keycxV47OKNroS5ID&maxRecords=9999&view=Current%20Members%20(Public)",function(v){
-			var r = v.records.sort(function(){return 0.5 - Math.random() });
-			$.each(r,function(i,el){
-				if(el.fields.Logo){
-					$("#member-data").append("<div class='member col-xs-6 col-md-3'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
-				}
-			});			
-		});
-	}
+	
 	if($("#temp-data").length){
 		$.getJSON("https://www.imonnit.com/json/SensorList/dGhlc3VtbWl0OkNoYXNwMTAxMA==",function(v){
 			$.each(v.Result,function(i,el){
@@ -24,6 +15,7 @@
 });
 var map = "";
 $(document).ready(function(){
+	if($("#member-data").length){loadMembers()}
 	var h = $(window).height() - $(".navbar-brand img").height() - 75;
 	map = $("#Map").mapplic({
 		sidebar: false,
@@ -34,3 +26,14 @@ $(document).ready(function(){
 		hovertip: false
 	});
 });
+
+function loadMembers(){
+		$.getJSON("https://api.airtable.com/v0/appf7AikwGn1IfZMA/CookSpring%20Members?api_key=keycxV47OKNroS5ID&maxRecords=9999&view=Current%20Members%20(Public)",function(v){
+			var r = v.records.sort(function(){return 0.5 - Math.random() });
+			$.each(r,function(i,el){
+				if(el.fields.Logo){
+					$("#member-data").append("<div class='member col-xs-6 col-md-3'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
+				}
+			});			
+		});	
+}
