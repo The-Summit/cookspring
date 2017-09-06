f94773efee47b4c38d83e019039e7b69e9ee0d20
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2017-09-06 12:57:07 -0400

    a better grid - more to come

--- js/script.js
+++ js/script.js
@@ -16,24 +16,54 @@
 var map = "";
 $(document).ready(function(){
 	if($("#member-data").length){loadMembers()}
-	var h = $(window).height() - $(".navbar-brand img").height() - 75;
-	map = $("#Map").mapplic({
-		sidebar: false,
-		height: h,
-		markers: false,
-		fullscreen: false,
-		fillcolor: false,
-		hovertip: false
-	});
+	if($("#Map").length){
+		var h = $(window).height() - $(".navbar-brand img").height() - 75;
+		map = $("#Map").mapplic({
+			sidebar: false,
+			height: h,
+			markers: false,
+			fullscreen: false,
+			fillcolor: false,
+			hovertip: false
+		});
+	}
 });
 
 function loadMembers(){
 		$.getJSON("https://api.airtable.com/v0/appf7AikwGn1IfZMA/CookSpring%20Members?api_key=keycxV47OKNroS5ID&maxRecords=9999&view=Current%20Members%20(Public)",function(v){
 			var r = v.records.sort(function(){return 0.5 - Math.random() });
+			var masterCls = [];
 			$.each(r,function(i,el){
 				if(el.fields.Logo){
-					$("#member-data").append("<div class='member col-xs-6 col-md-3'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
+					$.each(el.fields.Industry,function(i,e){
+						masterCls.indexOf(e) === -1 ? masterCls.push(e) : "";
+					});
+					var cls = el.fields.Industry.toString().replace(/[^a-z\,]+/ig,"-").replace(","," ").toLowerCase();
+					$("#member-data").append("<div class='member col-xs-6 col-md-3 " + cls +"'><a href='" + el.fields.Website + "'><img class='img-responsive' src='" + el.fields.Logo[0].url + "' /></a></div>");
 				}
+			});
+			$.each(masterCls,function(i,e){
+				var data = e.replace(/[^a-z\,]+/ig,"-").toLowerCase()
+				$(".holder .btn-group-vertical").append('<button class="btn btn-group btn-default" data-filter=".'+ data+'">' + e +'</button>');
+			})
+			$grid = $("#member-data").isotope({
+				itemSelector: '.member',
+				layoutMode: 'fitRows'
+			});
+			$grid.imagesLoaded().progress( function() {
+			  $grid.isotope('layout');
+			});
+			$(".holder .btn-group-vertical").on("click", "button",function(){
+				var filterValue = $( this ).attr('data-filter');
+				console.log(filterValue);
+				$grid.isotope({filter:filterValue});
+			});
+			$(".holder .btn-group-vertical").each( function( i, buttonGroup ) {
+				var $buttonGroup = $( buttonGroup );
+				$buttonGroup.on( 'click', 'button', function() {
+					$buttonGroup.find('.btn-success').removeClass('btn-success');
+					$( this ).addClass('btn-success');
+				});
 			});			
 		});	
 }
